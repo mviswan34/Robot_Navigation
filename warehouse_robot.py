dfb107af17e4fb0ece9f3dbcedd0b813816e6027
mviswan34 <114368620+mviswan34@users.noreply.github.com>
2023-04-29 06:15:02 -0400

    Update warehouse_robot.py

--- warehouse_robot.py
+++ warehouse_robot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import streamlit as st
-#numeric action codes: 0 = up, 1 = right, 2 = down, 3 = left
+#Trials with multiple mazes
 def maze1():
     aisles = {i: [] for i in range(11)}
     aisles[0] = [0,1,2,4,6,7,10]
@@ -51,7 +51,7 @@ al = maze1()
 #Create a 2D numpy array to hold the rewards for each state. 
 def initialize_rewards(environment_rows, environment_columns,aisles):
     rw = np.full((environment_rows, environment_columns), -100.)
-    rw[0, 5] = 100.  # Set the reward for the packaging area (i.e., the goal) to 100
+    rw[0, 6] = 100.  # Set the reward for the packaging area (i.e., the goal) to 100
     for row_index in range(1, 10):
         for column_index in aisles[row_index]:
             rw[row_index, column_index] = -1.
@@ -59,19 +59,22 @@ def initialize_rewards(environment_rows, environment_columns,aisles):
 
 environment_rows = 11
 environment_columns = 11
+#Set the initial Q-Values
 q_values = np.zeros((environment_rows, environment_columns, 4))
+#numeric action codes: 0 = up, 1 = right, 2 = down, 3 = left
 actions = ['up', 'right', 'down', 'left']
+#Get the reward matrix based on the environment defined.
 rewards = initialize_rewards(environment_rows, environment_columns,al)
 
 
-#define a function that determines if the specified location is a terminal state
+#Check if the state is a terminal state or not
 def is_terminal_state(current_row_index, current_column_index):
   if rewards[current_row_index, current_column_index] == -1.:
     return False
   else:
     return True
 
-#define a function that will choose a random, non-terminal starting location
+#Select a random starting location which is not terminal
 def get_starting_location():
   current_row_index = np.random.randint(environment_rows)
   current_column_index = np.random.randint(environment_columns)
@@ -80,14 +83,14 @@ def get_starting_location():
     current_column_index = np.random.randint(environment_columns)
   return current_row_index, current_column_index
 
-#define an epsilon greedy algorithm that will choose which action to take next (i.e., where to move next)
+#The epsilon greedy algorithm to choose which action to take next
 def get_next_action(current_row_index, current_column_index, epsilon):
   if np.random.random() < epsilon:
     return np.argmax(q_values[current_row_index, current_column_index])
   else: 
     return np.random.randint(4)
 
-#define a function that will get the next location based on the chosen action
+#Get the next location based on the chosen action
 def get_next_location(current_row_index, current_column_index, action_index):
   new_row_index = current_row_index
   new_column_index = current_column_index
@@ -101,21 +104,20 @@ def get_next_location(current_row_index, current_column_index, action_index):
     new_column_index -= 1
   return new_row_index, new_column_index
 
-#Define a function that will get the shortest path between any location within the obstacle course that 
-#the robot is allowed to travel and the item packaging location.
+#Define a function that will get the shortest path between the specified location and the destination.
 def get_shortest_path(start_row_index, start_column_index):
-  #return immediately if this is an invalid starting location
+  #Return empty if the location is that of a terminal state
   if is_terminal_state(start_row_index, start_column_index):
     return []
-  else: #if this is a 'legal' starting location
+  else:
     current_row_index, current_column_index = start_row_index, start_column_index
     shortest_path = []
     shortest_path.append([current_row_index, current_column_index])
-    #continue moving along the path until we reach the goal (i.e., the item packaging location)
+    #While until you reach the destination
     while not is_terminal_state(current_row_index, current_column_index):
-      #get the best action to take
+      #Take the best action
       action_index = get_next_action(current_row_index, current_column_index, 1.)
-      #move to the next location on the path, and add the new location to the list
+      #Move to the next state and add the new location to the shortest_path list
       current_row_index, current_column_index = get_next_location(current_row_index, current_column_index, action_index)
       shortest_path.append([current_row_index, current_column_index])
     return shortest_path
